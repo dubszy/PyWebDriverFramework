6e5ed5245d951b0222a4823c7646cbdeaaaf2183
Martin Waltman <martin@mkwiseman.com>
2018-12-10 07:08:00 -0500

    Update .gitignore; add setup; fix some small issues

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='PyWebDriverFramework',
+      version='0.0.1',
+      description='Selenium WebDriver wrapper framework',
+      packages=['wdframework', 'wdframework.loadables'])
